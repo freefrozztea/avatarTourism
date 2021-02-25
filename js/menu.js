5685b010adc70bb5053edf9cd9c906c2829efea5
freefrozztea <65555485+freefrozztea@users.noreply.github.com>
2021-02-25 13:07:55 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -14,7 +14,10 @@ const navSlide = () => {
             } else {
             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.2}s`;
             }
-        }); 
+        });
+        
+        //Burger Animation
+        hamburger.classList.toggle('toggle');
     });
 }
 

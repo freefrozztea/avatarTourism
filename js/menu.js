ff41fd46cad31f6b51b4cb182554ea69ea1abaef
freefrozztea <65555485+freefrozztea@users.noreply.github.com>
2021-02-25 12:08:11 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,20 @@
 const navSlide = () => {
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li'); 
 
     hamburger.addEventListener('click', () => {
+        //Toggle Nav
         nav.classList.toggle('nav-active');
+        
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.2}s`;
+            }
+        }); 
     });
 }
 
